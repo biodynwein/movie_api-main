35fe0d57bc797be08dba82c7eff640bedd6721df
biodynwein <96068785+biodynwein@users.noreply.github.com>
2022-05-07 08:26:24 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+//Import Express & Morgan
+const express = require('express'),
+  morgan = require('morgan');
+
+const app = express();
+
+let topTenMovies = [
+  {
+    title: 'Equilibrium',
+  },
+  {
+    title: 'Lords of Dogtown',
+  },
+  {
+    title: 'Lost Highway',
+  }
+];
+
+//Middleware functions
+app.use(morgan('common'));
+app.use(express.static('public'));
+
+//Endpoints
+app.get('/', (req, res) => {
+  res.send('Welcome to myFlix!');
+});
+
+app.get('/movies', (req, res) => {
+  res.json(topTenMovies);
+});
+
+//Error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+//Listening for port 8080
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
