9a2e6da1ec78a4abdb3d75a90edd941f4bca8bbd
biodynwein <96068785+biodynwein@users.noreply.github.com>
2022-06-17 14:43:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -29,6 +29,22 @@ app.get('/movies', (req, res) => {
   res.json(topTenMovies);
 });
 
+// GET data about a single movie by title to the user
+app.get('/movies/title', (req, res) => {
+  res.send('Return a list of ALL movies to the user');
+});
+
+// GET data about a genre by name/title
+app.get('/movies/genres/title', (req, res) => {
+  res.send('Return data about a genre (description) by name/title (e.g., “Thriller”)');
+});
+
+// GET data about a director by name
+app.get('/movies/directors/name', (req, res) => {
+  res.send('Return data about a director (bio, birth year, death year) by name');
+});
+
+
 //Error handling
 app.use((err, req, res, next) => {
   console.error(err.stack);
