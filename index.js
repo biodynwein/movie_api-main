6da5c53363c928cf71aad24d2215266311e267b2
biodynwein <96068785+biodynwein@users.noreply.github.com>
2022-08-06 08:26:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const Users = Models.User;
 const Genres = Models.Genre;
 const Director = Models.Director; 
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 app.use(bodyParser.json());
